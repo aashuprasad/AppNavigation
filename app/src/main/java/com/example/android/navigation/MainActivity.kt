790f0708b3426f46779606cad9d2dc59465060d9
Ashu Prasad <aashuprasad@live.com>
2022-11-18 11:09:39 +0530

    Added up navigation and Menu option

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,6 +19,8 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -26,5 +28,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        //Finding controller from the ID of NavHostFragment using KTX extension function.
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        //Link NavController to our ActionBar
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    //Activity handles navigateUp action from our Activity.
+    //For this we override onSupportNavigateUp, find nav controller and then
+    //call navigateUp()
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
